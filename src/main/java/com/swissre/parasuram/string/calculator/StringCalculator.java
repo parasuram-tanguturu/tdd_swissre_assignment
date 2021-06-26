d69283b7879ee8b5a0e40b231727e33f26bf8bfa
Parasuram <parasuram.tanguturu@siemens.com>
2021-06-26 13:43:35 +0530

    enhanced for SumOnMultipleNumbersWithSpaceDelimiter ExceptionOnNegativeNumbers

--- src/main/java/com/swissre/parasuram/string/calculator/StringCalculator.java
+++ src/main/java/com/swissre/parasuram/string/calculator/StringCalculator.java
@@ -1,18 +1,31 @@
 package com.swissre.parasuram.string.calculator;
 
 import java.util.Arrays;
-
+import java.util.List;
+import java.util.stream.Collectors;
+//Assumptions
+// -given string contains only numbers
+// -using delimiters : comma ,whitespace
 public class StringCalculator {
     public static int add(String input) {
         if(null==input || input.isEmpty()){
             return  0;
         }else if(input.contains(",")) {
-            final String[] nums = input.split(",");
-            return Arrays.stream(nums)
-                    .map(StringCalculator::toInt)
-                    .reduce(0, Integer::sum);
+            return getListOfPositiveNumbersFromString(input,",").stream().reduce(0,Integer::sum);
+        }else if(input.contains(" ")) {
+            return  getListOfPositiveNumbersFromString(input," ").stream().reduce(0,Integer::sum);
         }else {
             return toInt(input);
+        }
+    }
+    private static List<Integer> getListOfPositiveNumbersFromString(String input,String delimiter) {
+        List<Integer> numbersList = Arrays.stream(input.split(delimiter))
+                .map(StringCalculator::toInt).collect(Collectors.toList());
+        final List<Integer> negativeNumbersList = numbersList.stream().filter(n -> n < 0).collect(Collectors.toList());
+        if(negativeNumbersList.size()>0){
+            throw new RuntimeException("negative numbers not allowed : "+negativeNumbersList.stream().map(String::valueOf).collect(Collectors.joining(",")));
+        }else{
+            return numbersList;
         }
     }
 
--- src/test/java/com/swissre/parasuram/string/calculator/StringCalculatorTest.java
+++ src/test/java/com/swissre/parasuram/string/calculator/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package com.swissre.parasuram.string.calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class StringCalculatorTest {
 
     @Test
@@ -26,5 +27,20 @@ public class StringCalculatorTest {
         Assertions.assertEquals(10,StringCalculator.add("5,3,2"));
         Assertions.assertEquals(10,StringCalculator.add("3,2,3,2"));
         Assertions.assertEquals(10,StringCalculator.add("1,2,2,3,2"));
+    }
+    @Test
+    public void shouldReturnSumOnMultipleNumbersWithSpaceDelimiter(){
+        Assertions.assertEquals(10,StringCalculator.add("5 3 2"));
+        Assertions.assertEquals(10,StringCalculator.add("3 2 3 2"));
+        Assertions.assertEquals(10,StringCalculator.add("1 2 2 3 2"));
+    }
+    @Test
+    public void shouldRaiseExceptionOnNegativeNumbers(){
+        try{
+            StringCalculator.add("-1,2,3,4,-5");
+            Assertions.fail("exception expected");
+        }catch (RuntimeException ex){
+            Assertions.assertEquals("negative numbers not allowed : -1,-5",ex.getMessage());
+        }
     }
 }
